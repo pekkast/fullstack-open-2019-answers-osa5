97ca81ba8af67cf10bb5aea3ba0fc9490227d134
Pekka Turtola <pekka.turtola@yap.fi>
2019-07-30 01:50:11 +0300

    Complete 5.6

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Blog.scss';
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+const Blog = ({ blog }) => {
+  const [view, setView] = useState('list');
+
+  if (view === 'list') {
+    return (
+      <div className="blog-item">
+        <h3 onClick={() => setView('details')}>{blog.title} &ndash; {blog.author}</h3>
+      </div>
+    );
+  }
+
+  return (
+    <div className="blog-item">
+      <h3 onClick={() => setView('list')}>{blog.title} &ndash; {blog.author}</h3>
+      <p>
+        <a href={blog.url}>{blog.url}</a><br />
+        {blog.likes} likes <button>Like</button>
+      </p>
+    </div>
+  );
+};
 
 export default Blog;
